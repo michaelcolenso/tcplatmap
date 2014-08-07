f76829b41e888e022b7be03fad380d5cc6831c38
michael colenso <github@michaelcolenso.com>
2014-08-07 01:08:00 -0400

    ga

--- public/js/main.js
+++ public/js/main.js
@@ -120,4 +120,13 @@ $(document).ready(function() {
       this.stream.point(point.x, point.y);
     }
   });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53567676-1', 'auto');
+  ga('send', 'pageview');
+
 });
