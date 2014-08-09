83d77475145d3a1a8c2ab6b6ca0bd145baa8732c
michael colenso <github@michaelcolenso.com>
2014-08-09 17:31:57 -0400

    dokku push

--- app.js
+++ app.js
@@ -66,10 +66,6 @@ var MongoClient = require('mongodb').MongoClient;
 
 var csrfExclude = ['/url1', '/url2'];
 
-/**
- * Express configuration.
- */
-
 app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
