a56a329552d918354cf9ad8e1b172fd51abaf9be
michael colenso <github@michaelcolenso.com>
2014-08-09 19:52:47 -0400

    added worker to Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: node app.js
+
+worker: node controllers/getsales.js
--- controllers/getsales.js
+++ controllers/getsales.js
@@ -1,9 +1,10 @@
 var request = require('request');
 var parser = require('JSONStream').parse('features.*.attributes');
 var db = process.env.MONGO_URL;
+
 var options = { db: db, collection: 'sales' };
 var streamToMongo = require('stream-to-mongo')(options);
-
+console.log(db);
 request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
   .pipe(parser)
   .pipe(streamToMongo);
