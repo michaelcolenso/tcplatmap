1ea9aa48f0f610a09df3988c132f931c99e0e34b
michael colenso <github@michaelcolenso.com>
2014-08-09 19:01:05 -0400

    mongo bs

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -1,11 +1,8 @@
 var request = require('request');
-var parser = require('JSONStream').parse('features.*.attributes')
-var options = { db: process.env.MONGO_URL , collection: 'sales' };
+var parser = require('JSONStream').parse('features.*.attributes');
+var db = process.env.MONGO_URL;
+var options = { db: db, collection: 'sales' };
 var streamToMongo = require('stream-to-mongo')(options);
-
-// request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
-//   .pipe(fs.createWriteStream("public/js/salesdatatc.json"));
-
 
 request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
   .pipe(parser)
