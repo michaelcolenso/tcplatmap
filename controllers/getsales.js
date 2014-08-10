542e8dc712d238cbbe11108ade288e6a603e1db1
michael colenso <github@michaelcolenso.com>
2014-08-09 20:28:53 -0400

    fix syntax err

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -2,12 +2,11 @@ var request = require('request');
 var parser = require('JSONStream').parse('features.*.attributes');
 var db = process.env.MONGO_URL;
 
-var options = { db: db, collection: 'sales' };
-var streamToMongo = require('stream-to-mongo')(options);
+//var options = { db: db, collection: 'sales' };
+//var streamToMongo = require('stream-to-mongo')(options);
 
 request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json", function(err, response, body) {
   if (!err && response.statusCode == 200) {
     console.log(body);
-  } else {
-    console.log('wtf bro');
+  }
 });
