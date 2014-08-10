3d395bc7460cdd67c5b2a541abf887e4dda0161f
michael colenso <github@michaelcolenso.com>
2014-08-09 21:31:52 -0400

    fuck

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -1,10 +1,12 @@
 var request = require('request');
 var parser = require('JSONStream').parse('features.*.attributes');
-var fs = require('fs');
 var db = process.env.MONGO_URL;
-var sales = fs.createReadStream(__dirname + '/../sales.json');
+var fs = require('fs');
 
 var options = { db: db, collection: 'sales' };
 var streamToMongo = require('stream-to-mongo')(options);
 
-sales.pipe(parser).pipe(streamToMongo);
+var file = fs.createReadStream(__dirname + '/sales.json');
+console.log(file);
+
+file.pipe(parser).pipe(streamToMongo);
