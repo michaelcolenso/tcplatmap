a3ba97843e3bebc34a53b881359d8a785a7982f5
michael colenso <github@michaelcolenso.com>
2014-08-09 20:15:00 -0400

    wtf

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -5,7 +5,8 @@ var db = process.env.MONGO_URL;
 var options = { db: db, collection: 'sales' };
 var streamToMongo = require('stream-to-mongo')(options);
 console.log(db);
-request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
-  .pipe(parser)
-  .pipe(streamToMongo);
+request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json", function(err, req, res) {
+  if (err) { console.log(err) }
+  console.log(res.statusCode);
+});
 console.log('got it bro');
