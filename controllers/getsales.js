505ff9a2832ab4cda241bbf8dbe609cd3154a8c9
michael colenso <github@michaelcolenso.com>
2014-08-10 23:15:48 -0400

    this

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -1,8 +1,11 @@
+var secrets = require('../config/secrets');
 var request = require('request');
+var fs = require('fs');
 var parser = require('JSONStream').parse('features.*.attributes')
-var options = { db: 'mongodb://localhost:27017/test', collection: 'sales' };
+var options = { db: secrets.db,  collection: 'tcsalesdata' };
 var streamToMongo = require('stream-to-mongo')(options);
 
-request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
-  .pipe(parser)
-  .pipe(streamToMongo);
+fs.createReadStream('salesdatatc.json').pipe(parser).pipe(streamToMongo);
+//request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
+  //.pipe(parser)
+  //.pipe(streamToMongo);
